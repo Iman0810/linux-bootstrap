c4e3f0f74d17337ac5ce0b84202db73e1bdb0846
Iman0810 <iman.chandrasiri0810@gmail.com>
2026-08-13 12:00:27 +0530

    cli go added

---
+++ internal/cli/cli.go
@@ -0,0 +1,109 @@
+package cli
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/Iman0810/linux-bootstrap/internal/packages"
+	"github.com/Iman0810/linux-bootstrap/internal/runner"
+	"github.com/Iman0810/linux-bootstrap/internal/system"
+)
+
+func Run() {
+	if len(os.Args) < 2 {
+		printUsage()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "info":
+		runInfo()
+
+	case "setup":
+		runSetup(os.Args[2:])
+
+	default:
+		fmt.Printf("Unknown command: %s\n\n", command)
+		printUsage()
+	}
+}
+
+func runInfo() {
+	osInfo, err := system.GetOSInfo()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	packageManager := packages.DetectManager(osInfo)
+
+	fmt.Println("Linux Bootstrap")
+	fmt.Println("----------------")
+	fmt.Println("OS:", osInfo.Name)
+	fmt.Println("Version:", osInfo.Version)
+	fmt.Println("ID:", osInfo.ID)
+	fmt.Println("Based on:", osInfo.IDLike)
+	fmt.Println("Package Manager:", packageManager)
+}
+
+func runSetup(args []string) {
+	setupFlags := flag.NewFlagSet("setup", flag.ExitOnError)
+
+	dryRun := setupFlags.Bool(
+		"dry-run",
+		false,
+		"Show commands without executing them",
+	)
+
+	setupFlags.Parse(args)
+
+	osInfo, err := system.GetOSInfo()
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	packageManager := packages.DetectManager(osInfo)
+
+	r := runner.Runner{
+		DryRun: *dryRun,
+	}
+
+	manager := packages.GetPackageManager(packageManager, r)
+
+	if manager == nil {
+		fmt.Println("Unsupported package manager:", packageManager)
+		return
+	}
+
+	fmt.Println("Linux Bootstrap Setup")
+	fmt.Println("----------------------")
+	fmt.Println("OS:", osInfo.Name)
+	fmt.Println("Package Manager:", packageManager)
+	fmt.Println("Dry Run:", *dryRun)
+
+	fmt.Println()
+
+	err = manager.Update()
+	if err != nil {
+		fmt.Println("Update failed:", err)
+		return
+	}
+
+	err = manager.Install("git", "curl", "wget")
+	if err != nil {
+		fmt.Println("Installation failed:", err)
+		return
+	}
+}
+
+func printUsage() {
+	fmt.Println("Linux Bootstrap")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Println("  linux-bootstrap info")
+	fmt.Println("  linux-bootstrap setup [--dry-run]")
+}
--- main.go
+++ main.go
@@ -1,50 +1,12 @@
 package main
 
 import (
-	"fmt"
-	"log"
 
-	"github.com/Iman0810/linux-bootstrap/internal/packages"
-	"github.com/Iman0810/linux-bootstrap/internal/runner"
-	"github.com/Iman0810/linux-bootstrap/internal/system"
+
+	"github.com/Iman0810/linux-bootstrap/internal/cli"
+
 )
 
 func main() {
-	osInfo, err := system.GetOSInfo()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	packageManager := packages.DetectManager(osInfo)
-
-	r := runner.Runner{
-		DryRun: true,
-	}
-
-	manager := packages.GetPackageManager(packageManager, r)
-
-	fmt.Println("Linux Bootstrap")
-	fmt.Println("----------------")
-	fmt.Println("OS:", osInfo.Name)
-	fmt.Println("Version:", osInfo.Version)
-	fmt.Println("ID:", osInfo.ID)
-	fmt.Println("Based on:", osInfo.IDLike)
-	fmt.Println("Package Manager:", packageManager)
-
-	if manager == nil {
-		log.Fatal("Unsupported package manager")
-	}
-
-	fmt.Println("Package manager initialized successfully")
-
-	fmt.Println("\nTesting package manager...")
-
-	err = manager.Update()
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = manager.Install("git", "curl", "wget")
-	if err != nil {
-	log.Fatal(err)
-}
+	cli.Run()
 }
